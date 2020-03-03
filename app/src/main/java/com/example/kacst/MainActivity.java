12f870cba1b8ba84f513a8566daff610213dde47
benzir106 <shovo.cse@gmail.com>
2020-03-04 01:15:59 +0600

    KACST

---
+++ app/src/main/java/com/example/kacst/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.kacst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.web);
+
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String url="https://www.khanjahanalicst.edu.bd";
+        webView.loadUrl(url);
+
+    }
+    public class MyViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+
+}
